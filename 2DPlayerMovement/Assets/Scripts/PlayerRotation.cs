b7f881af9f3030a7565a41822106df7b7d47f7f0
Moeketsi Moloi <moloimason@gmail.com>
2022-09-27 11:02:35 -0700

    update

--- 2DPlayerMovement/Assets/Scripts/PlayerRotation.cs
+++ 2DPlayerMovement/Assets/Scripts/PlayerRotation.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerRotation : MonoBehaviour
 {
-
     public float speed = 5;
     public float rotationSpeed = 20;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -19,8 +14,8 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector2.right * Time.deltaTime * horizontalInput * speed);
         transform.Translate(Vector2.up * Time.deltaTime * verticalInput * speed);
-        //transform.Rotate(Vector3.forward, verticalInput * rotationSpeed * Time.deltaTime);
+        //transform.Translate(Vector2.up * Time.deltaTime * verticalInput * speed);
+        transform.Rotate(Vector3.back, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
